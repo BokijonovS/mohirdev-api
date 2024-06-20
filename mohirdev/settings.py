094348fe6cf543d8fed77958b2aa42d77df173d2
BokijonovS <sanatjon.dark@gmail.com>
2024-06-20 21:23:13 +0500

    secret_key updated

--- mohirdev/settings.py
+++ mohirdev/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tm91$=v3&3zg$3qj8!fa^f5hcyso#=n=@6suyfam8%r+_+gdm-'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
