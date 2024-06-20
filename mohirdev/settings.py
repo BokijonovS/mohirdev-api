e9482a75ef6a13f1fa22d4a935d8a6aa3c32602e
BokijonovS <sanatjon.dark@gmail.com>
2024-06-20 21:21:51 +0500

    secret_key updated

--- mohirdev/settings.py
+++ mohirdev/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-tm91$=v3&3zg$3qj8!fa^f5hcyso#=n=@6suyfam8%r+_+gdm-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
@@ -81,12 +82,26 @@ WSGI_APPLICATION = 'mohirdev.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mohirdev',
+        'USER': 'postgres',
+        'PASSWORD': '0990',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
