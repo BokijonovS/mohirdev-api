c8acfb45e68a7b134f9887a26a6da0809b022c52
BokijonovS <sanatjon.dark@gmail.com>
2024-06-21 09:51:27 +0500

    permission classes updated

--- app/views.py
+++ app/views.py
@@ -15,7 +15,7 @@ class CourseViewSet(viewsets.ModelViewSet):
     '''This viewset does all the work on model Course'''
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class LessonViewSet(viewsets.ModelViewSet):
@@ -26,18 +26,21 @@ class LessonViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['name']
     search_fields = ['name']
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class VideoViewSet(viewsets.ModelViewSet):
     '''This viewset does all the work on model Video'''
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     '''This viewset does all the work on model Comment'''
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
